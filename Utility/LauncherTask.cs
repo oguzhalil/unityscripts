73c87659ff61c7307363e1912ffe529ea3e63f24
Oguzhan <playablegraphs@gmail.com>
2019-06-10 20:09:13 +0300

    Launcher is completed.

---
+++ Utility/LauncherTask.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class LauncherTask : MonoBehaviour 
+{
+    public abstract bool IsDone ();
+}
+
---
+++ Utility/TestLauncherTask.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLauncherTask : LauncherTask
+{
+    private bool m_IsDone;
+
+    private void Start ()
+    {
+        Invoke( "PrintHelloWorld" , 2.0f );
+    }
+
+    private void PrintHelloWorld()
+    {
+        Debug.Log( "HelloWorld." );
+        m_IsDone = true;
+    }
+
+    public override bool IsDone ()
+    {
+        return m_IsDone;
+    }
+}
